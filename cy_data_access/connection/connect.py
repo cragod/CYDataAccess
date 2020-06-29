4dbb87b76aa4fbe70d9e05323db339807b4cd020
hyl <hyl4@meitu.com>
2020-06-30 00:29:48 +0800

    add connect from env vars func

--- cy_data_access/connection/connect.py
+++ cy_data_access/connection/connect.py
@@ -1,3 +1,4 @@
+import os
 from pymodm.connection import connect
 
 # ==== Configuration ====
@@ -8,7 +9,11 @@ CN_SEQUENCE = 'sequence'
 CN_CCXT_CONFIG = 'cxt'
 
 
-def connect_db(name, password, ip, db_name):
+def connect_db(user, password, host, db_name):
     # 连接到数据库
-    uri = "mongodb://{}:{}@{}/{}?authSource=admin".format(name, password, ip, db_name)
+    uri = "mongodb://{}:{}@{}/{}?authSource=admin".format(user, password, host, db_name)
     connect(uri, db_name)
+
+
+def connect_db_env(host, db_name):
+    connect_db(os.environ['DB_MNR_USER'], os.environ['DB_MNR_PWD'], host, db_name)
