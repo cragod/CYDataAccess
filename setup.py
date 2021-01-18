e6d22a35e34def52831c11c119a4dceffff1e358
hyl <hyl4@meitu.com>
2021-01-18 23:47:46 +0800

    Bump version: 0.4.11 → 0.4.12

--- setup.py
+++ setup.py
@@ -52,6 +52,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/cragod/CYDataAccess',
-    version='0.4.11',
+    version='0.4.12',
     zip_safe=False,
 )
