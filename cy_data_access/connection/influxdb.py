b6b6c1d3a3e455408732c6f0c1f688697084ec4f
hyl <hyl4@meitu.com>
2021-02-07 00:18:07 +0800

    delete client after performent

--- cy_data_access/connection/influxdb.py
+++ cy_data_access/connection/influxdb.py
@@ -5,6 +5,7 @@ from influxdb_client.client.write_api import SYNCHRONOUS
 def influx_client_query_data_frame(host='http://localhost:8086', query=''):
     client = InfluxDBClient(url=host, token="", org="", debug=False)
     result = client.query_api().query_data_frame(org="", query=query)
+    client.__del__()
     return result
 
 
@@ -12,12 +13,13 @@ def influx_client_query_data_frame(host='http://localhost:8086', query=''):
     """ 查询数据 """
     client = InfluxDBClient(url=host, token="", org="", debug=False)
     result = client.query_api().query_data_frame(org="", query=query)
+    client.__del__()
     return result
 
 
 def influx_client_write_data_frame(host='http://localhost:8086', df=None, df_time_index_name='', database='', measurement_name='', tag_columns=[]):
     """ 保存数据 """
-    client = InfluxDBClient(url="http://localhost:8086", token="", org="", debug=True)
+    client = InfluxDBClient(url=host, token="", org="", debug=False)
     write_client = client.write_api(write_options=WriteOptions(batch_size=500,
                                                                flush_interval=10_000,
                                                                jitter_interval=2_000,
@@ -27,3 +29,4 @@ def influx_client_write_data_frame(host='http://localhost:8086', df=None, df_tim
                                                                exponential_base=2))
     df.set_index(df_time_index_name, inplace=True)
     write_client.write(database, record=df, data_frame_measurement_name=measurement_name, data_frame_tag_columns=tag_columns)
+    write_client.__del__()
